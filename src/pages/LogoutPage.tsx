17d59002ee7e64375550c93db0d829ef1ea11b0a
priyashan1 <priyashan@xgengroup.com.au>
2024-09-28 09:06:31 +0530

    implementation of logout page

--- src/pages/LogoutPage.tsx
+++ src/pages/LogoutPage.tsx
@@ -5,15 +5,22 @@ const LogoutPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-col justify-center bg-cover items-center h-screen gap-3">
+    <div className="flex">
+      <div className=" flex-1 flex flex-col justify-center bg-cover items-center h-screen gap-3">
         <img src="src/assets/logout-page.jpg" />
         <button
           onClick={() => navigate("/")}
-          className="bg-blue-600 text-2xl w-1/6 text-white font-semibold py-2 px-4 rounded-3xl shadow hover:bg-blue-700 transition duration-200"
+          className="bg-blue-600 text-2xl w-2/5 text-white font-semibold py-2 px-4 rounded-3xl shadow hover:bg-blue-700 transition duration-200"
         >
           Back To the Home
         </button>
+      </div>
+
+      <div className="flex  flex-1 justify-end bg-cover items-center gap-3 ">
+        <img
+          src="src/assets/infosec-login.png"
+          className=" border-8 border-black rounded-xl border-r-0"
+        />
       </div>
     </div>
   );
